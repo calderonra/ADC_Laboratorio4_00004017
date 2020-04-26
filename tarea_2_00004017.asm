2aa7ed3583c52612f84e531bdd49b771bdb06876
calderonra <00004017@uca.edu.sv>
2020-04-26 16:58:47 -0600

    Ejercicio 2 :o

--- tarea_2_00004017.asm
+++ tarea_2_00004017.asm
@@ -1,6 +1,44 @@
 org	100h
 
 section .text
+
+
+	;ejercicio 2
+
+	mov	ax,2d
+	mov	bx,0000h
+	mov	cx,0000h
+	mov	dx,0000h
+	mov	cx,2d
+	mov	bx, 0h
+	mul	cx
+	mov	[bx+210h],ax
+
+loop: inc bx
+	mul cx
+	mov[bx+210h], ax
+	cmp ax, 0xff
+	jb loop
+
+loopOverlow: inc bx
+	inc bx
+	mul cx 
+	mov [bx+210h], ax 
+	cmp bx, 12d
+	jb loopOverlow
+	
+
+;Ejercicio 3 
+
+	mov	ax,0000h
+	mov	bx,0000h
+	mov	cx,0000h
+	mov	dx,0000h
+	
+	mov	
+
+	;Ejercicio 1
+
 	mov     ax, 4d
         mov     bx, 0d
 	add     ax, bx ;AX = 4
@@ -30,3 +68,4 @@ section .data
 
 comnt	db	"Hay salud"
 len	equ	$-comnt
+
