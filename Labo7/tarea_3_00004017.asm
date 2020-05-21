59f2aeb2b9d64069845cfa8a1e2e4f37735bcf49
calderonra <00004017@uca.edu.sv>
2020-05-20 20:54:13 -0600

    mensaje mostrado y captura subida

--- Labo7/tarea_3_00004017.asm
+++ Labo7/tarea_3_00004017.asm
@@ -1,54 +1,104 @@
-	org 100h
-
+  
+	org 	100h
 
 section .text
 
-	call	texto
-	call	cursor
-	call	phrase
+	call 	texto	
+	call 	cursor
+	call 	phrase
 	call	kbwait
 
-	int 20h
+	int 	20h
 
-texto: mov	ah,00h
-       mov	al,03h
-       int	10h
-       ret
-
-cursor: mov	ah,01h
-	mov	ch,00000000b
-	mov	cl,00001110b
-
-	int	10h
+texto:	mov 	ah, 00h
+	mov	al, 03h
+	int 	10h
 	ret
 
-w_char: mov	ah,09h
-	mov	al,cl
-	mov	bh,0h
-	mov	bl,00001111b
-	mov	cx,1h
+cursor: mov	ah, 01h
+	mov 	ch, 00000000b
+	mov 	cl, 00001110b
+	int 	10h
 	ret
 
-kbwait: mov	ax,0000h
-	int	16h
+w_char:	mov 	ah, 09h
+	mov 	al, cl
+	mov 	bh, 0h
+	mov 	bl, 00001111b
+	mov 	cx, 1h
+	int 	10h
 	ret
 
-m_cursr:mov	ah,02h
-	mov	dl,29d
-	mov	dh,12d
-	mov	bh,00h
-	int	10h
+kbwait: mov 	ax, 0000h
+	int 	16h
 	ret
 
-phrase: mov	di,29d
-lupi:   mov	cl,[msg+di-29d]
-	call	m_cursr
-	call	w_char
+m_cursr:mov 	ah, 02h
+	mov 	dx, di  ; columna
+        add     dx, ax
+	mov 	dh, bh ; fila
+	mov 	bh, 0h
+	int 	10h
+	ret
+
+phrase:	mov 	di, 0d
+lupi:	mov 	cl, [msg+di]
+        mov 	bh, 9d 
+        mov     ax, 20d
+	call    m_cursr
+	call 	w_char
 	inc	di
-	cmp	di,len
+	cmp 	di, len
 	jb	lupi
-	ret
+    
+ 	mov 	di, 0d
 
-section .data
-msg	db	"Hola mundo"
-len	equ	$-msg+29d
+
+lupi2:	mov 	cl, [msg2+di]
+        mov 	bh, 12d ; fila
+        mov     ax, 20d; columna
+        call    m_cursr
+        call 	w_char
+        inc	di
+        cmp 	di, len2
+        jb	lupi2
+        
+	mov di, 0d
+
+lupi3:	mov 	cl, [msg3+di]
+        mov 	bh, 15d ; fila
+        mov     ax, 20d; columna
+        call    m_cursr
+        call 	w_char
+        inc	di
+        cmp 	di, len3
+        jb	lupi3
+
+	mov di, 0d
+
+lupi4:	mov 	cl, [msg4+di]
+        mov 	bh, 18d ; fila
+        mov     ax, 20d; columna
+        call    m_cursr
+        call 	w_char
+        inc	di
+        cmp 	di, len4
+        jb	lupi4
+        ret
+
+section .data1
+msg	db 	"Ella no esta buscando novio "
+len 	equ	$-msg
+
+section .data2
+msg2	db 	"Quiere salir a joder "
+len2 	equ	$-msg2
+
+section .data3
+msg3	db 	"Quiere olvidarse de ese bobo"
+len3 	equ	$-msg3
+
+section .data3
+msg4	db 	"Porque el cabron le fue infiel"
+len4 	equ	$-msg4
+
